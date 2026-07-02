4a0066342dbf68067711e2f5c9a622133f5f3c42
Pranish Chaulagain <153913535+Pranish03@users.noreply.github.com>
2026-07-02 10:30:13 +0545

    Finish running migrations

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+using BlogApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogApp.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Post> Posts { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Comment> Comments { get; set; }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,18 @@
+using BlogApp.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+                       ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+});
 
 var app = builder.Build();
 
